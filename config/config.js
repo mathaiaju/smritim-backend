c6a29718c04b5905c9bef8e7ea1b5fb8ced3a82a
mathaiaju <mathaiaju@gmail.com>
2026-01-09 08:42:24 +0530

    Change default DB username and password

--- config/config.js
+++ config/config.js
@@ -2,8 +2,8 @@ require('dotenv').config();
 module.exports = {
   db: {
     host: process.env.DB_HOST || 'localhost',
-    username: process.env.DB_USER || 'aju',
-    password: process.env.DB_PASS || 'admin123',
+    username: process.env.DB_USER || 'changeit',
+    password: process.env.DB_PASS || 'changeit',
     database: process.env.DB_NAME || 'adr_chatbot',
     port: process.env.DB_PORT || 3306,
     dialect: 'mysql'
